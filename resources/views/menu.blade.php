b12eb9c49f7050d50532b7746df98d5174996aa2
Ridayanti Wardani <ridayantiwardani@gmail.com>
2024-12-17 18:27:23 +0700

    banner.blade.php

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,54 @@
+<!-- Awal MENU NAVBAR -->
+<div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div>
+            <ul class="navbar-nav">
+                <!-- Home -->
+                <li class="nav-item">
+                    <a class="nav-link" href="/home">Home</a>
+                </li>
+
+                <!-- Data Users -->
+                <li class="nav-item">
+                    <a class="nav-link" href="/users">Data Pengguna</a>
+                </li>
+
+                <!-- Data Flights -->
+                <li class="nav-item">
+                    <a class="nav-link" href="/flights">Data Penerbangan</a>
+                </li>
+
+                <!-- Data Airports -->
+                <li class="nav-item">
+                    <a class="nav-link" href="/airports">Data Bandara</a>
+                </li>
+
+                <!-- Data Airlines -->
+                <li class="nav-item">
+                    <a class="nav-link" href="/airlines">Data Maskapai</a>
+                </li>
+
+                <!-- Data Bookings -->
+                <li class="nav-item">
+                    <a class="nav-link" href="/bookings">Data Pemesanan</a>
+                </li>
+
+                <!-- Data Payments -->
+                <li class="nav-item">
+                    <a class="nav-link" href="/payments">Data Pembayaran</a>
+                </li>
+
+                <!-- Data Passengers -->
+                <li class="nav-item">
+                    <a class="nav-link" href="/passengers">Data Penumpang</a>
+                </li>
+
+                <!-- Logout -->
+                <li class="nav-item">
+                    <a class="nav-link" href="/logout">Logout</a>
+                </li>
+            </ul>
+        </div>
+    </nav>            
+</div>
+<!-- Akhir MENU NAVBAR -->

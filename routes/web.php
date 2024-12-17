6b390c75dca9da933ae766364ccf6ec015e7b71c
Ridayanti Wardani <ridayantiwardani@gmail.com>
2024-12-17 15:34:04 +0700

    perubahan

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/*Route::get('/', function () {
+Route::get('/', function () {
     return view('welcome');
-});*/
+});
 Route::resource('/posts', \App\Http\Controllers\PostController::class);
